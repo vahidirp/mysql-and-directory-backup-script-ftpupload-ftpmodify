ba5eb946b89596246fd419fa54a263ccd125aa11
-VAHID-IRP <community.vahid@hotmail.com>
2023-11-04 00:29:12 +0330

    Delete rootdirectory.sh

--- rootdirectory.sh
+++
@@ -1,6 +0,0 @@
-#Domains/tabairan   tabairan
-DATE=$(date +"%Y-%m-%d_%H-%M-%S")
-DIR1="/home/webdata-backup/files/tabairan/"
-mkdir -p "$DIR1"
-zip -r $DIR1$DATE.zip /home/Domains/tabairan
-find $DIR1. \*.zip -mtime +3 -delete
