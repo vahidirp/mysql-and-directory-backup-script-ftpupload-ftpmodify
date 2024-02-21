dd0f2ff8a3d81410e24a885563efc4fb18897a1f
-VAHID-IRP <community.vahid@hotmail.com>
2024-02-21 22:43:20 +0330

    Update mysqlbackup.sh
    fix bug save backup file without .sql format

--- docker/mysqlbackup.sh
+++ docker/mysqlbackup.sh
@@ -27,14 +27,16 @@ EMAIL_SUBJECT_FAILED="Database Backup Failed"
 mkdir -p "$DB_DIR_1"
 mkdir -p "$DB_DIR_2"
 # Docker exec command to run mysqldump inside the container and compress the backup
- docker exec -it $DB_CONTAINER_1 mysqldump --user=$DB1_User --password=$DB1_Password $DB_Name_1 | gzip > $DB_DIR_1/$DB_Name_1-${DATE}.gz
- docker exec -it $DB_CONTAINER_2 mysqldump --user=$DB2_User --password=$DB2_Password $DB_Name_2 | gzip > $DB_DIR_2/$DB_Name_2-${DATE}.gz
+docker exec $DB_CONTAINER_1 mysqldump --user="DB1_User" --password="$DB1_Password" $DB_NAME_1 > $DB_DIR_1/$DB_NAME_1-$DATE.sql
+gzip $DB_DIR_1/$DB_NAME_1-$DATE.sql
+docker exec $DB_CONTAINER_2 mysqldump --user="DB2_User" --password="$DB2_Password" $DB_NAME_2 > $DB_DIR_2/$DB_NAME_2-$DATE.sql
+gzip $DB_DIR_2/$DB_NAME_2-$DATE.sql
  if [ $? -eq 0 ]; then
   echo "MySQL/MariaDB database backup completed successfully" | mail -s "$EMAIL_SUBJECT_PASS" -a "From: $SENDER_EMAIL" -s smtp=smtp://$SMTP_SERVER:$SMTP_PORT -s smtp-use-starttls -s smtp-auth=login -s smtp-auth-user=$SMTP_USER -s smtp-auth-password=$SMTP_PASSWORD $RECIPIENT_EMAIL
 else
   echo "MySQL/MariaDB database backup failed" | mail -s "$EMAIL_SUBJECT_FAILED" -a "From: $SENDER_EMAIL" -s smtp=smtp://$SMTP_SERVER:$SMTP_PORT -s smtp-use-starttls -s smtp-auth=login -s smtp-auth-user=$SMTP_USER -s smtp-auth-password=$SMTP_PASSWORD $RECIPIENT_EMAIL
 fi
-# Use the 'find' command to locate and delete old backup files (4 days or older)
+# Use the find command to locate and delete old backup files (4 days or older)
 find "$DB_DIR_1" -ctime +4 -exec rm -rf {} \;
 find "$DB_DIR_2" -ctime +4 -exec rm -rf {} \;
 # Optionally, you can print a message indicating the cleanup is complete
