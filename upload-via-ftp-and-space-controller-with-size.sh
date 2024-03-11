5d580b53ef15eca33534174d5ef9de096a68c909
-VAHID-IRP <community.vahid@hotmail.com>
2024-03-11 13:48:21 +0330

    Update upload-via-ftp-and-space-controller-with-size.sh
    use ncftpd instead of lftp method and create directory if not exist is not automatically it's just manually

--- upload-via-ftp-and-space-controller-with-size.sh
+++ upload-via-ftp-and-space-controller-with-size.sh
@@ -9,7 +9,7 @@ ftp_directory="/app&web-backup/vahid.community" #change this variables to direct
 local_directory="/path/to/local/directory"  #change this variable to local directory of your backup files
 
 # Maximum allowed space in megabytes
-max_space_mb=1000 #change this variables to find files that are larger more than something 1024mb=fix1GB
+LIMIT_MB=100 #change this variables to find files that are larger more than something 1024mb=fix1GB
 
 # SMTP configuration for sending email
 SMTP_SERVER="your_smtp_server" # Change this variables
@@ -21,36 +21,18 @@ SENDER_EMAIL="sender@example.com" # Can same as RECIPIENT_EMAIL
 EMAIL_SUBJECT="Upload To FTP" 
 message="The upload and cleanup process is complete."
 
-# Connect to the FTP server
-lftp -u "$ftp_user","$ftp_pass" "$ftp_server" <<EOF
-    # Change to the FTP directory
-    cd "$ftp_directory"
+# Get the current used space in the remote directory
+USED_SPACE=$(ncftp -u "$ftp_user" -p "$ftp_pass" "$ftp_server" -c "du -sm $FTPD" | awk '{print $1}')
 
-    # Get the total size of files in the directory
-    total_size=$(du -ms | awk '{print $1}')
+# Calculate the remaining available space
+AVAILABLE_SPACE=$((LIMIT_MB - USED_SPACE))
 
-    # Check if the total size exceeds the maximum allowed space
-    if [ "$total_size" -gt "$max_space_mb" ]; then
-        echo "Total space used ($total_size MB) exceeds the limit ($max_space_mb MB). Taking action..."
-
-        # List files in the directory
-        ls
-
-        # You can implement actions here, such as deleting older files or notifying someone.
-
-    else
-        echo "Total space used ($total_size MB) is within the limit ($max_space_mb MB). No action needed."
-    fi
-
-    # Exit FTP session
-    exit
-EOF
-
-# Create the remote directory if it doesn't exist
-lftp -e "mkdir -p $ftp_directory; bye" -u "$ftp_user","$ftp_password" "$ftp_server"
-
-# Upload files and directories
-lftp -e "mirror -R --reverse --delete-first $local_directory $ftp_directory; bye" -u "$ftp_user","$ftp_password" "$ftp_server"
+if [ "$AVAILABLE_SPACE" -lt 0 ]; then
+    echo "Error: Space limit exceeded!"
+    exit 1
+else
+    echo "Available space: $AVAILABLE_SPACE MB"
+fi
 
 # Send email notification
 echo "$message" | mail -s "$EMAIL_SUBJECT" -a "From: $SENDER_EMAIL" -s smtp=smtp://$SMTP_SERVER:$SMTP_PORT -s smtp-use-starttls -s smtp-auth=login -s smtp-auth-user=$SMTP_USER -s smtp-auth-password=$SMTP_PASSWORD $RECIPIENT_EMAIL
